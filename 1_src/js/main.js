5e70ea15ada06c0189058763f80f370a0f161626
Jovan <jovan226@mail.ru>
2021-08-31 18:57:51 +0800

    Catalog menu js

--- 1_src/js/main.js
+++ 1_src/js/main.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     tabs('.product-tabs', '.product-tabs__title', '.product-tabs__block')
 
-    const accordion = (tabsSel, headerSel, contentSel) => {
+    const accordion = (tabsSel, headerSel, contentSel, paddings = 0, firstActive = 0) => {
         let tabs = document.querySelector(tabsSel)
 
         if (tabs) {
@@ -69,17 +69,24 @@ document.addEventListener('DOMContentLoaded', () => {
             let content = tabs.querySelectorAll(contentSel)
 
             let lastIndex = 0
-            let paddings = 14
+            let padding = paddings
+
+            header[firstActive].classList.add('active')
+            content[firstActive].classList.add('active')
+            content[firstActive].style.maxHeight = content[firstActive].scrollHeight + padding + 'px'
 
             for (let i = 0; i < header.length; i++) {
                 header[i].addEventListener('click', () => {
                     if (!(header[i].classList.contains('active'))) {
                         header[lastIndex].classList.remove('active')
+                        content[lastIndex].classList.remove('active')
                         content[lastIndex].style.maxHeight = '0px'
+                        content[i].classList.add('active')
                         header[i].classList.add('active')
-                        content[i].style.maxHeight = content[i].scrollHeight + paddings + 'px'
+                        content[i].style.maxHeight = content[i].scrollHeight + padding + 'px'
                         lastIndex = i
                     } else {
+                        content[i].classList.remove('active')
                         header[i].classList.remove('active')
                         content[i].style.maxHeight = '0px'
                         lastIndex = i
@@ -89,5 +96,60 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    accordion('.js-accordion', '.js-accordion__title', '.js-accordion__content')
+    accordion('.js-accordion', '.js-accordion__title', '.js-accordion__content', 14)
+
+
+    const mainMenu = (tabsSel, headerSel, contentSel, closeSel, overlaySel, paddings = 0, firstActive = 0) => {
+        let tabs = document.querySelector(tabsSel)
+
+        if (tabs) {
+            console.log('asda', tabs)
+            let header = tabs.querySelectorAll(headerSel)
+            let content = tabs.querySelectorAll(contentSel)
+            let close = tabs.querySelectorAll(closeSel)
+            let overlay = document.querySelector(overlaySel)
+
+            let lastIndex = 0
+            let padding = paddings
+
+            openMenu(firstActive)
+
+            overlay.addEventListener('click', () => {
+                closeMenu(lastIndex)
+            })
+
+            for (let i = 0; i < header.length; i++) {
+                header[i].addEventListener('click', () => {
+                    if (!(header[i].classList.contains('active'))) {
+                        closeMenu(lastIndex)
+                        openMenu(i)
+                        lastIndex = i
+                    } else {
+                        closeMenu(i)
+                        lastIndex = i
+                    }
+                })
+
+                close[i].addEventListener('click', () => {
+                    closeMenu(i)
+                })
+            }
+
+            function closeMenu(index) {
+                header[index].classList.remove('active')
+                content[index].classList.remove('active')
+                content[index].style.maxHeight = '0px'
+                overlay.classList.remove('active')
+            }
+
+            function openMenu(index) {
+                content[index].classList.add('active')
+                header[index].classList.add('active')
+                content[index].style.maxHeight = content[index].scrollHeight + padding + 'px'
+                overlay.classList.add('active')
+            }
+        }
+    }
+
+    mainMenu('.js-menu', '.js-menu__title', '.js-menu__content', '.js-menu__close', '.main-menu__overlay', 38)
 })
